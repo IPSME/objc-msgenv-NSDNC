daa225c187fd9ec2a86d0a4f10c8d98f437ba785
knev <knev@users.noreply.github.com>
2023-09-29 21:57:07 +0200

    style: clean up code.

--- IPSME_MsgEnv.mm
+++ IPSME_MsgEnv.mm
@@ -17,36 +17,7 @@ NSString* gk_MSG= @"msg";
 
 //-----------------------------------------------------------------------------------------------------------------------------------------
 
-// The second NSString* is NULL, unless the Reflector hack below is used
-//
-//namespace NSDNC {
-//	typedef void (*tp_callback)(id id_msg, void* p_void, NSString* UNUSED);
-//}
-
-@interface IPSME_MsgEnv_NSDNC : NSObject
-
-@end
-
-// This hack allows for an object to be sent,
-// which in turn can be used to remove ricocheted messages
-// e.g., send a UUID and drop incoming messages with that UUID
-//
-@interface IPSME_MsgEnv_NSDNC (Reflector)
-
-+ (void) publish:(id)msg withObject:(NSString*)object;
-
-@end
-
-//-----------------------------------------------------------------------------------------------------------------------------------------
-
-//NSMutableDictionary* mdic_ = [NSMutableDictionary dictionary];
-
-struct t_assoc {
-	IPSME_MsgEnv::tp_callback p_callback;
-	void* p_void;
-};
-
-typedef std::unordered_map<IPSME_MsgEnv::tp_callback, t_assoc> t_umap;
+typedef std::unordered_map<IPSME_MsgEnv::tp_callback, void*> t_umap;
 t_umap umap_;
 
 void notification_callback(CFNotificationCenterRef center,
@@ -70,13 +41,8 @@ void notification_callback(CFNotificationCenterRef center,
 	@try {
 		IPSME_MsgEnv::tp_callback p_callback= (IPSME_MsgEnv::tp_callback)p_observer;
 		
-//		NSValue* nsval_k= [NSValue valueWithPointer:(void *)p_callback];
-//		NSValue* nsval_v= [mdic_ objectForKey:nsval_k];
-//		assert (nsval_v != nil);
-//		void* p_void= [nsval_v pointerValue];
-
 		assert(umap_.find(p_callback) != umap_.end());
-		void* p_void= umap_[p_callback].p_void;
+		void* p_void= umap_[p_callback];
 		
 		p_callback([nsstr_msg UTF8String], p_void);
 	}
@@ -86,53 +52,20 @@ void notification_callback(CFNotificationCenterRef center,
 	}
 }
 
+//-----------------------------------------------------------------------------------------------------------------------------------------
+
 // Sandboxing:
 //		https://stackoverflow.com/questions/36687525/nsdistributednotificationcenter-not-working
 //		https://developer.apple.com/forums/thread/129437
 // Example: https://github.com/RabbitMC/DistributedNotifications/blob/master/DistributedNotifications/NotificationsManager/NotificationsManager.m
 
-/*
-@implementation IPSME_MsgEnv_NSDNC
-
-@end
-
-@implementation IPSME_MsgEnv_NSDNC (Reflector)
-
-
-// Even though object is a (const void*), if you don't pass an NSString*,
-// you get an access exception.
-//
-+ (void) publish:(id)id_msg withObject:(NSString*)object
-{
-    NSDictionary* nsdic= @{
-        gk_MSG : id_msg
-    };
-    
-	CFNotificationCenterRef ncref_Distributed = CFNotificationCenterGetDistributedCenter();
-	CFNotificationCenterPostNotification(ncref_Distributed,			    	// CFNotificationCenterRef center
-                                         (__bridge CFStringRef)gk_NAME,     // CFNotificationName name
-										 (__bridge CFStringRef)object,  	// const void *object
-                                         (__bridge CFDictionaryRef)nsdic,   // CFDictionaryRef userInfo
-										 YES						    	// Boolean deliverImmediately
-		);
-}
-
-@end
-*/
-
-//-----------------------------------------------------------------------------------------------------------------------------------------
 
 RET_TYPE IPSME_MsgEnv::subscribe(IPSME_MsgEnv::tp_callback p_callback, void* p_void)
 {
-	t_assoc assoc = {p_callback, p_void};
-	umap_[p_callback]= assoc;
+	umap_[p_callback]= p_void;
 	
 	@autoreleasepool
 	{
-//		NSValue* nsval_k = [NSValue valueWithPointer:(void*)p_callback];
-//		NSValue* nsval_v = [NSValue valueWithPointer:p_void];
-//		[mdic_ setObject:nsval_v forKey:nsval_k];
-		
 		// Even though *object is a (const void*), if you don't pass an NSString*,
 		// you get an access exception.
 		//
@@ -159,9 +92,6 @@ RET_TYPE IPSME_MsgEnv::unsubscribe(IPSME_MsgEnv::tp_callback p_callback)
 										   (__bridge CFStringRef)gk_NAME,       // CFNotificationName name
 										   NULL					    	    	// const void *object
 			);
-		
-//		NSValue* nsval_key= [NSValue valueWithPointer:(void*)p_callback];
-//		[mdic_ removeObjectForKey:nsval_key];
 	}
 	
 	umap_.erase(p_callback);
@@ -188,6 +118,39 @@ RET_TYPE IPSME_MsgEnv::publish(const char* psz_msg)
 
 	return 0;
 }
+
+// This hack allows for an object to be sent,
+// which in turn can be used to remove ricocheted messages
+// e.g., send a UUID and drop incoming messages with that UUID
+//
+/*
+
+@interface IPSME_MsgEnv_NSDNC (Reflector)
++ (void) publish:(id)msg withObject:(NSString*)object;
+@end
+ 
+@implementation IPSME_MsgEnv (Reflector)
+
+// Even though object is a (const void*), if you don't pass an NSString*,
+// you get an access exception.
+//
++ (void) publish:(id)id_msg withObject:(NSString*)object
+{
+	NSDictionary* nsdic= @{
+		gk_MSG : id_msg
+	};
+	
+	CFNotificationCenterRef ncref_Distributed = CFNotificationCenterGetDistributedCenter();
+	CFNotificationCenterPostNotification(ncref_Distributed,			    	// CFNotificationCenterRef center
+										 (__bridge CFStringRef)gk_NAME,     // CFNotificationName name
+										 (__bridge CFStringRef)object,  	// const void *object
+										 (__bridge CFDictionaryRef)nsdic,   // CFDictionaryRef userInfo
+										 YES						    	// Boolean deliverImmediately
+		);
+}
+
+@end
+*/
 
 RET_TYPE IPSME_MsgEnv::process_requests(int i_timeout) {
 	return 0;
